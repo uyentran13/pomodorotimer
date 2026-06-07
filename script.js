5ebebd11f4a950933c0807aec66445b38dad3c97
Uyên <tnuyen13@gmail.com>
2026-06-07 02:54:16 +0200

    Change pomodoro timer duration to 60 minutes
    Updated pomodoro timer duration from 25 minutes to 60 minutes.

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 // Global variables
-let timeLeft = 25 * 60; // seconds
+let timeLeft = 60 * 60; // seconds
 let timerInterval;
 let currentInterval = 'pomodoro';
 let backgroundColor = '#F1F1EF'; // Default background color
@@ -22,7 +22,7 @@ const saveBtn = document.getElementById('save-btn');
 // Event listeners for interval buttons
 pomodoroIntervalBtn.addEventListener('click', () => {
   currentInterval = 'pomodoro';
-  timeLeft = 25 * 60;
+  timeLeft = 60 * 60;
   updateTimeLeftTextContent();
 });
 
@@ -52,7 +52,7 @@ startStopBtn.addEventListener('click', () => {
 resetBtn.addEventListener('click', () => {
   stopTimer();
   if (currentInterval === 'pomodoro') {
-    timeLeft = 25 * 60;
+    timeLeft = 60 * 60;
   } else if (currentInterval === 'short-break') {
     timeLeft = 5 * 60;
   } else {
